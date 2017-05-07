418eea5a29e3be4ea322acb4cea50f5d8ccb5429
khai96_ <hvksmr1996@gmail.com>
2017-05-07 22:19:06 +0700

    Log

--- addons/ppas.sh
+++ addons/ppas.sh
@@ -2,3 +2,7 @@ sudo add-apt-repository ppa:ubuntu-wine/ppa -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
+
+echo 'Search for Wine'
+apt-cache search wine | grep wine
+true
